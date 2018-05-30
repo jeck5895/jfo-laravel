e8c4b7ce8255f10d0223cca96ede043562d8f5ea
Jerick Labasan <jeck.labasan@gmail.com>
2018-05-30 18:19:58 +0800

    Added migrations registration api for Jobseeker

--- app/Employer.php
+++ app/Employer.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Employer extends Model
 {
     protected $guarded = [];
+
+    public static function boot() 
+    {
+        parent::boot();
+
+        self::creating(function ($model) {
+            $model->uuid = (string) Uuid::generate(4);
+        });
+    }
 }
--- app/Jobseeker.php
+++ app/Jobseeker.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Jobseeker extends Model
 {
     protected $guarded = [];
+
+    public static function boot() 
+    {
+        parent::boot();
+
+        self::creating(function ($model) {
+            $model->uuid = (string) Uuid::generate(4);
+        });
+    }
 }
+
